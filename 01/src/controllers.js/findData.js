1a40705a0286e3cfa12f2bfb7dc7bf380f70935d
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-09-14 02:52:52 -0300

    Adicionado arquivo json

--- 01/src/controllers.js/findData.js
+++ 01/src/controllers.js/findData.js
@@ -1,9 +1,36 @@
 const getCompany = require('../abstract')
+const fs = require('fs/promises')
 
 const findData = async (req, res) => {
   const { dominioEmpresa } = req.query
-  const data = await getCompany(dominioEmpresa)
-  return res.json(data)
+  try {
+    const data = await getCompany(dominioEmpresa)
+
+    if (data.name !== null && data.name !== undefined) {
+      const dataJSON = await fs.readFile('./01/src/companies.json')
+
+      if (dataJSON.length > 0) {
+        const dataARRAY = JSON.parse(dataJSON)
+        const companyAlreadyExists = dataARRAY.find((company) => {
+          return company.domain === String(dominioEmpresa)
+        })
+        if (!companyAlreadyExists) {
+          dataARRAY.push(data)
+          await fs.writeFile('./01/src/companies.json', JSON.stringify(dataARRAY))
+        }
+      } else {
+        const dataARRAY = []
+        dataARRAY.push(data)
+        await fs.writeFile('./01/src/companies.json', JSON.stringify(dataARRAY))
+      }
+    } 
+    
+    return res.json(data)
+    
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500).json({message: 'Internal server error.'})
+  }
 }
 
 module.exports = findData
